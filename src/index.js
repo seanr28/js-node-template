eb059e8125a7aac1b35e5f7b3a358c0ccfbeb81a
Sean Riesenberger <sean.riesenberger@student.swic.edu>
2025-03-13 20:10:06 -0500

    Add function practice examples

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const x = 3
+const student1 = {
+  name: 'Sarah',
+  course: 'JavaScript',
+  grade: 95,
+}
+const student2 = {
+  name: 'James',
+  course: 'English',
+  grade: 85,
+}
+const student3 = {
+  name: 'Trisha',
+  course: 'Algebra',
+  grade: 99,
+}
 
-console.log(x)
+function displayStudentInfo(student) {
+  return `${student.name} is taking ${student.course} and expects to earn ${student.grade}%.`
+}
+
+console.log(displayStudentInfo(student1))
+console.log(displayStudentInfo(student2))
+console.log(displayStudentInfo(student3))
