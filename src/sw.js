ca0bfcef41083c8d1a5b61e2cc0882b6a020fc0d
Sean Riesenberger <sean.riesenberger@student.swic.edu>
2025-04-24 23:32:20 -0500

    update then function to await

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,9 @@
-const owen = fetch('https://www.swapi.tech/api/people/6')
+const response = await fetch('https://www.swapi.tech/api/people/6')
+const data = await response.json() // Convert the response to JSON
+console.log(data) // Log the data to the console
 
-console.log(owen)
+//fetch('https://www.swapi.tech/api/people/6')
+//.then((response) => response.json())
+//.then((data) => {
+//console.log(data)
+//})
